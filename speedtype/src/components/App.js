1fad6fce80d4cba14e0fc7925261115443db04fc
CalWal <callumtrwallace@gmail.com>
2020-01-27 13:33:43 +1000

    basic App layout

--- speedtype/src/components/App.js
+++ speedtype/src/components/App.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <h1> Hello World </h1>;
+  return (
+    <div>
+      <h1>Test Your Speed</h1>
+      <textarea value="To begin game, start typing!" />
+      <h4>Time Remaining:???</h4>
+      <button>Start Game</button>
+
+      <h1>Word Count:???</h1>
+    </div>
+  );
 };
 
 export default App;
