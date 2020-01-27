a7fd4e7a011316f1454095ff8cd1cedd85469162
CalWal <callumtrwallace@gmail.com>
2020-01-28 07:09:38 +1000

    Using hooks: now handles input change, word count + countdown.

--- speedtype/src/components/App.js
+++ speedtype/src/components/App.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(10);
+
+  const handleChange = e => {
+    const { value } = e.target;
+    setText(value);
+  };
+
+  const wordCount = text => {
+    const wordsArr = text.trim().split(" ");
+    return wordsArr.filter(word => word !== "").length;
+  };
+
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(function() {
+        setTimeRemaining(time => time - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
   return (
     <div>
       <h1>Test Your Speed</h1>
-      <textarea value="To begin game, start typing!" />
-      <h4>Time Remaining:???</h4>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time Remaining:{timeRemaining}</h4>
       <button>Start Game</button>
-
-      <h1>Word Count:???</h1>
+      <h1>Word Count:</h1>
     </div>
   );
 };
